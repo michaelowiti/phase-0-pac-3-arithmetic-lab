2763e0b8b60c47f82e87b96b72cf1ed428db5729
mike <mjowiti@gmail.com>
2023-06-09 00:58:13 +0300

    almost completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+    return a+b;
+
+}
+function subtract(a,b){
+    return a-b;
+
+}
+function multiply(a,b){
+    return a*b;
+
+}
+function divide(a,b){
+    return a/b;
+
+}
+
+function increment(n){
+    return (n+1);
+}
+function decrement(n){
+    return (n-1);
+}
+function preserveDecimal(String){
+    return String;
+}
+function makeInt(string){
+    return String;
+}
+function makeInt(n,){
+    return n;
+}
+function preserveDecimal(n){
+    return n.string;
+}
+
